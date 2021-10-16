5c4810bbdaa34a150f4d9f7b5b2e48e13fd0a1ad
Josh Choo <dev@joshuous.com>
2021-10-16 17:01:46 +0800

    fix: Spawn app in the background
    Awaiting the spawn_app would indefinitely run the app but
    not yield control to the remaining test code. Therefore, it
    would cause the test to hang without a request being sent.
    Use tokio::spawn to run the app in the background and avoid
    blocking the request.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use actix_web::dev::Server;
 use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 
 // A type implements Responder if it can be converted to HttpResponse
@@ -12,8 +13,11 @@ async fn health_check() -> impl Responder {
     HttpResponse::Ok().finish()
 }
 
-pub async fn run() -> std::io::Result<()> {
-    HttpServer::new(|| {
+// Return a Result to the Server, which the caller can .await.
+// If we choose to await here, it would be extremely difficult to run this
+// function in tokio::spawn (not sure why).
+pub fn run() -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(|| {
         App::new()
             // Routes combines Handlers with a set of Guards
             // "/" implements the Guard trait and passes the request on only if it fulfils.
@@ -23,6 +27,6 @@ pub async fn run() -> std::io::Result<()> {
             .route("/{name}", web::get().to(greet))
     })
     .bind("127.0.0.1:8000")?
-    .run()
-    .await
+    .run();
+    Ok(server)
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,12 +1,16 @@
 // Decouple our app from the rest of the test.
-async fn spawn_app() -> std::io::Result<()> {
-    zero2prod::run().await
+fn spawn_app() {
+    let server = zero2prod::run().expect("Failed to bind address");
+    // tokio::spawn will await Futures that it receives.
+    // tokio::spawn drops the task when the tokio runtime shuts down, so we don't
+    // need to worry about our Server persisting after the tests finish.
+    tokio::spawn(server);
 }
 
 // These tests are not coupled to our app, besides the spawn_app call.
 #[actix_rt::test]
 async fn health_check_works() {
-    spawn_app().await.expect("Failed to spawn app");
+    spawn_app();
     let client = reqwest::Client::new();
     let response = client
         .get("http://127.0.0.1:8000/health_check")
