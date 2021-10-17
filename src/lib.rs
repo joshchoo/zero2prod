a3838bcab676c99ef3bb151b3d904bb0def7ce2e
Josh Choo <dev@joshuous.com>
2021-10-17 23:00:22 +0800

    refactor!: Remove greet API

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 use actix_web::dev::Server;
-use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 use serde::Deserialize;
 use std::net::TcpListener;
 
@@ -13,21 +13,11 @@ pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
             // "/" implements the Guard trait and passes the request on only if it fulfils.
             // web::get() is short for Route::new().guard(guard::Get()) and passes only GET requests through to the handler
             .route("/health_check", web::get().to(health_check))
-            .route("/", web::get().to(greet))
-            .route("/{name}", web::get().to(greet))
             .route("/subscriptions", web::post().to(subscribe))
     })
     .listen(listener)?
     .run();
     Ok(server)
-}
-
-// A type implements Responder if it can be converted to HttpResponse
-async fn greet(req: HttpRequest) -> impl Responder {
-    // Opinion: not too fond of .get("name") not being type-checked against .route("/{name}").
-    // Problematic if we change "name" is the handler/route but not the other.
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", &name)
 }
 
 async fn health_check() -> impl Responder {
