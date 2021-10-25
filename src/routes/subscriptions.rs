fda88f4b7b7d5d96bed0d514af69e3d8746b6846
Josh Choo <dev@joshuous.com>
2021-10-25 20:57:19 +0800

    feat: Insert subscription into DB

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,5 +1,8 @@
 use actix_web::{web, HttpResponse};
+use chrono::Utc;
 use serde::Deserialize;
+use sqlx::PgPool;
+use uuid::Uuid;
 
 #[derive(Deserialize)]
 pub struct SubscriberData {
@@ -7,7 +10,29 @@ pub struct SubscriberData {
     name: String,
 }
 
-// Form extractor for x-www-form-urlencoded data
-pub async fn subscribe(_form: web::Form<SubscriberData>) -> HttpResponse {
-    HttpResponse::Ok().finish()
+pub async fn subscribe(
+    // Extract form data from x-www-form-urlencoded
+    form: web::Form<SubscriberData>,
+    // Extract PgConnection from application state
+    pool: web::Data<PgPool>,
+) -> HttpResponse {
+    match sqlx::query!(
+        r#"
+    INSERT INTO subscriptions (id, email, name, subscribed_at)
+    VALUES ($1, $2, $3, $4)
+    "#,
+        Uuid::new_v4(),
+        form.email,
+        form.name,
+        Utc::now()
+    )
+    .execute(pool.get_ref())
+    .await
+    {
+        Ok(_) => HttpResponse::Ok().finish(),
+        Err(e) => {
+            println!("Failed to execute query: {}", e);
+            HttpResponse::InternalServerError().finish()
+        }
+    }
 }
