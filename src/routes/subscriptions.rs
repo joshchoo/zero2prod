17c7d07299666f60f31fa09bd3acfbce3ff34e13
Josh Choo <dev@joshuous.com>
2021-10-30 16:09:22 +0800

    refactor: Replace logging in subscription with tracing

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -16,12 +16,23 @@ pub async fn subscribe(
     // Extract PgConnection from application state
     pool: web::Data<PgPool>,
 ) -> HttpResponse {
-    log::info!(
-        "Adding '{}' '{}' as a new subscriber",
+    let request_id = Uuid::new_v4();
+
+    // Logs the Adding a new subscriber.
+    let request_span = tracing::info_span!("Adding a new subscriber.", %request_id, subscriber_email = %form.email, subscriber_name = %form.name);
+    // Starts and logs the entry into span. When this variable drops, it will exit the span.
+    let _request_span_guard = request_span.enter();
+
+    tracing::info!(
+        "request_id {} - Adding '{}' '{}' as a new subscriber",
+        request_id,
         form.email,
         form.name
     );
-    log::info!("Saving new subscriber details in the database");
+    tracing::info!(
+        "request_id {} - Saving new subscriber details in the database",
+        request_id
+    );
     match sqlx::query!(
         r#"
     INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -36,11 +47,18 @@ pub async fn subscribe(
     .await
     {
         Ok(_) => {
-            log::info!("New subscriber details have been saved");
+            tracing::info!(
+                "request_id {} - New subscriber details have been saved",
+                request_id
+            );
             HttpResponse::Ok().finish()
         }
         Err(e) => {
-            log::error!("Failed to execute query: {:?}", e);
+            tracing::error!(
+                "request_id {} - Failed to execute query: {:?}",
+                request_id,
+                e
+            );
             HttpResponse::InternalServerError().finish()
         }
     }
