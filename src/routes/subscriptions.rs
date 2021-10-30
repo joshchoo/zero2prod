e4292e405bc43abe2635c9ea1338ab4bd05832d3
Josh Choo <dev@joshuous.com>
2021-10-30 16:32:29 +0800

    refactor: Instrument database query span
    Uses the Instrument trait from tracing-futures.
    
    Start a span
    -> Enter a span
    <- Exit a span
    -- Close the span
    
    Example output:
    
    [2021-10-30T08:28:08Z INFO  zero2prod::routes::subscriptions] Adding a new subscriber.; request_id=a9f12800-4db0-4dd4-9666-e68690eab76c subscriber_email=a@k.com subscriber_name=a
    [2021-10-30T08:28:08Z TRACE tracing::span::active] -> Adding a new subscriber.
    [2021-10-30T08:28:08Z INFO  tracing::span] Saving new subscriber details in the database
    [2021-10-30T08:28:08Z TRACE tracing::span::active] -> Saving new subscriber details in the database
    [2021-10-30T08:28:08Z TRACE tracing::span::active] <- Saving new subscriber details in the database
    [2021-10-30T08:28:08Z TRACE tracing::span::active] -> Saving new subscriber details in the database
    [2021-10-30T08:28:08Z TRACE tracing::span::active] <- Saving new subscriber details in the database
    [2021-10-30T08:28:08Z TRACE tracing::span::active] -> Saving new subscriber details in the database
    [2021-10-30T08:28:08Z INFO  sqlx::query] /* SQLx ping */; rows: 0, elapsed: 2.204ms
    [2021-10-30T08:28:08Z TRACE tracing::span::active] <- Saving new subscriber details in the database
    [2021-10-30T08:28:08Z TRACE tracing::span::active] -> Saving new subscriber details in the database
    [2021-10-30T08:28:08Z TRACE tracing::span::active] <- Saving new subscriber details in the database
    [2021-10-30T08:28:08Z TRACE tracing::span::active] -> Saving new subscriber details in the database
    [2021-10-30T08:28:08Z TRACE tracing::span::active] <- Saving new subscriber details in the database
    [2021-10-30T08:28:08Z TRACE tracing::span::active] -> Saving new subscriber details in the database
    [2021-10-30T08:28:09Z INFO  sqlx::query] INSERT INTO subscriptions (id, …; rows: 0, elapsed: 5.961ms
    
        INSERT INTO
          subscriptions (id, email, name, subscribed_at)
        VALUES
          ($1, $2, $3, $4)
    
    [2021-10-30T08:28:09Z TRACE tracing::span::active] <- Saving new subscriber details in the database
    [2021-10-30T08:28:09Z TRACE tracing::span] -- Saving new subscriber details in the database
    [2021-10-30T08:28:09Z TRACE tracing::span::active] <- Adding a new subscriber.
    [2021-10-30T08:28:09Z TRACE tracing::span] -- Adding a new subscriber.

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -2,6 +2,7 @@ use actix_web::{web, HttpResponse};
 use chrono::Utc;
 use serde::Deserialize;
 use sqlx::PgPool;
+use tracing_futures::Instrument;
 use uuid::Uuid;
 
 #[derive(Deserialize)]
@@ -23,16 +24,7 @@ pub async fn subscribe(
     // Starts and logs the entry into span. When this variable drops, it will exit the span.
     let _request_span_guard = request_span.enter();
 
-    tracing::info!(
-        "request_id {} - Adding '{}' '{}' as a new subscriber",
-        request_id,
-        form.email,
-        form.name
-    );
-    tracing::info!(
-        "request_id {} - Saving new subscriber details in the database",
-        request_id
-    );
+    let query_span = tracing::info_span!("Saving new subscriber details in the database");
     match sqlx::query!(
         r#"
     INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -44,15 +36,10 @@ pub async fn subscribe(
         Utc::now()
     )
     .execute(pool.get_ref())
+    .instrument(query_span)
     .await
     {
-        Ok(_) => {
-            tracing::info!(
-                "request_id {} - New subscriber details have been saved",
-                request_id
-            );
-            HttpResponse::Ok().finish()
-        }
+        Ok(_) => HttpResponse::Ok().finish(),
         Err(e) => {
             tracing::error!(
                 "request_id {} - Failed to execute query: {:?}",
