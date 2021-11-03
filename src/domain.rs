a99869539e79788d504d6eba4e90395615d034a5
Josh Choo <dev@joshuous.com>
2021-11-03 08:44:15 +0800

    test: Add unit tests for SubscriberName

--- src/domain.rs
+++ src/domain.rs
@@ -2,6 +2,7 @@ use unicode_segmentation::UnicodeSegmentation;
 
 // Keep the String field private to make it impossible to instantiate a SubscriberName
 // directly outside this module.
+#[derive(Debug)]
 pub struct SubscriberName(String);
 
 impl SubscriberName {
@@ -32,4 +33,48 @@ impl AsRef<str> for SubscriberName {
 pub struct NewSubscriber {
     pub email: String,
     pub name: SubscriberName,
+}
+
+#[cfg(test)]
+mod subscriber_name_tests {
+    use super::SubscriberName;
+    use claim::{assert_err, assert_ok};
+
+    #[test]
+    fn a_256_grapheme_long_name_is_valid() {
+        let name = "a".repeat(256);
+        assert_ok!(SubscriberName::parse(name));
+    }
+
+    #[test]
+    fn a_name_longer_than_256_graphemes_is_rejected() {
+        let name = "a".repeat(257);
+        assert_err!(SubscriberName::parse(name));
+    }
+
+    #[test]
+    fn whitespace_only_names_are_rejected() {
+        let name = " ".to_string();
+        assert_err!(SubscriberName::parse(name));
+    }
+
+    #[test]
+    fn empty_string_is_rejected() {
+        let name = "".to_string();
+        assert_err!(SubscriberName::parse(name));
+    }
+
+    #[test]
+    fn names_containing_an_invalid_character_are_rejected() {
+        for name in &['/', '(', ')', '"', '<', '>', '\\', '{', '}'] {
+            let name = name.to_string();
+            assert_err!(SubscriberName::parse(name));
+        }
+    }
+
+    #[test]
+    fn a_valid_name_is_parsed_successfully() {
+        let name = "Ursula Le Guin".to_string();
+        assert_ok!(SubscriberName::parse(name));
+    }
 }
