9baebe296551dc452cd90a7126e543b882f3aace
Josh Choo <dev@joshuous.com>
2021-11-06 12:25:50 +0800

    refactor: Extract server initialization logic to a build function

--- src/startup.rs
+++ src/startup.rs
@@ -1,10 +1,36 @@
+use crate::configuration::Settings;
 use crate::email_client::EmailClient;
 use crate::routes;
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
+use sqlx::postgres::PgPoolOptions;
 use sqlx::PgPool;
 use std::net::TcpListener;
 use tracing_actix_web::TracingLogger;
+
+/// Initializes database connections, email client, binds to TCP port and returns a Server.
+pub async fn build(configuration: Settings) -> Result<Server, std::io::Error> {
+    let connection_pool = PgPoolOptions::new()
+        .connect_timeout(std::time::Duration::from_secs(2))
+        .connect_lazy_with(configuration.database.with_db());
+    let sender_email = configuration
+        .email_client
+        .sender()
+        .expect("Invalid sender email address.");
+    let timeout = configuration.email_client.timeout();
+    let email_client = EmailClient::new(
+        configuration.email_client.base_url,
+        sender_email,
+        configuration.email_client.authorization_token,
+        timeout,
+    );
+    let address = format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port
+    );
+    let listener = TcpListener::bind(address)?;
+    run(listener, connection_pool, email_client)
+}
 
 // Return a Result to the Server, which the caller can .await.
 // If we choose to await here, it would be extremely difficult to run this
