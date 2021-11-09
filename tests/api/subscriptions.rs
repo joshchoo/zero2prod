cac933f3361601ce2b4eebc7d4ac002ce20c348c
Josh Choo <dev@joshuous.com>
2021-11-09 23:34:43 +0800

    test: Split subscribe test

--- tests/api/subscriptions.rs
+++ tests/api/subscriptions.rs
@@ -20,11 +20,20 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
     let response = app.post_subscriptions(body.into()).await;
 
     assert_eq!(200, response.status().as_u16());
+}
+
+#[actix_rt::test]
+async fn subscribe_persists_the_new_subscriber() {
+    let app = spawn_app().await;
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+
+    app.post_subscriptions(body.into()).await;
 
     let saved = sqlx::query!("SELECT email, name FROM subscriptions")
         .fetch_one(&app.db_pool)
         .await
         .expect("Failed to fetch saved subscription.");
+
     assert_eq!(saved.email, "ursula_le_guin@gmail.com");
     assert_eq!(saved.name, "le guin");
 }
