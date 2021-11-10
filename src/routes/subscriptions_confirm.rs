f63954586cdeacffd88986898a364ec275247137
Josh Choo <dev@joshuous.com>
2021-11-10 12:26:57 +0800

    feat: Complete subscription confirmation implementation
    - Mark subscriber as 'confirmed' in the subscriptions table.
    - Update sqlx-data.json

--- src/routes/subscriptions_confirm.rs
+++ src/routes/subscriptions_confirm.rs
@@ -1,13 +1,59 @@
 use actix_web::{web, HttpResponse};
+use sqlx::PgPool;
+use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
 pub struct Parameters {
-    #[allow(dead_code)] // Needed to validate that subscription_token is in the query parameters.
     subscription_token: String,
 }
 
 #[allow(clippy::async_yields_async)]
-#[tracing::instrument(name = "Confirm a pending subscriber", skip(_parameters))]
-pub async fn confirm(_parameters: web::Query<Parameters>) -> HttpResponse {
+#[tracing::instrument(name = "Confirm a pending subscriber", skip(parameters))]
+pub async fn confirm(parameters: web::Query<Parameters>, pool: web::Data<PgPool>) -> HttpResponse {
+    let subscriber_id =
+        match get_subscriber_id_from_token(&pool, &parameters.subscription_token).await {
+            Ok(Some(id)) => id,
+            Ok(None) => return HttpResponse::Unauthorized().finish(),
+            Err(_) => return HttpResponse::InternalServerError().finish(),
+        };
+
+    if confirm_subscriber(&pool, subscriber_id).await.is_err() {
+        return HttpResponse::InternalServerError().finish();
+    }
+
     HttpResponse::Ok().finish()
+}
+
+#[tracing::instrument(name = "Get subscriber_id from token", skip(pool, subscription_token))]
+async fn get_subscriber_id_from_token(
+    pool: &PgPool,
+    subscription_token: &str,
+) -> Result<Option<Uuid>, sqlx::Error> {
+    let result = sqlx::query!(
+        "SELECT subscriber_id FROM subscription_tokens WHERE subscription_token = $1",
+        subscription_token
+    )
+    // use fetch_xxx with SELECT statements
+    .fetch_optional(pool)
+    .await
+    .map_err(|e| {
+        tracing::error!("Failed to execute query: {:?}", e);
+        e
+    })?;
+    Ok(result.map(|r| r.subscriber_id))
+}
+
+#[tracing::instrument(name = "Mark subscriber as confirmed", skip(pool, subscriber_id))]
+async fn confirm_subscriber(pool: &PgPool, subscriber_id: Uuid) -> Result<(), sqlx::Error> {
+    sqlx::query!(
+        "UPDATE subscriptions SET status = 'confirmed' WHERE id = $1",
+        subscriber_id
+    )
+    .execute(pool)
+    .await
+    .map_err(|e| {
+        tracing::error!("Failed to execute query: {:?}", e);
+        e
+    })?;
+    Ok(())
 }
--- tests/api/subscriptions_confirm.rs
+++ tests/api/subscriptions_confirm.rs
@@ -17,6 +17,20 @@ async fn confirmations_without_token_are_rejected_with_a_400() {
 }
 
 #[actix_rt::test]
+async fn confirmations_with_invalid_token_are_rejected_with_a_401() {
+    let app = spawn_app().await;
+
+    let response = reqwest::get(&format!(
+        "{}/subscriptions/confirm?subscription_token=tokenNotInDatabase",
+        app.address
+    ))
+    .await
+    .unwrap();
+
+    assert_eq!(response.status().as_u16(), 401);
+}
+
+#[actix_rt::test]
 async fn the_link_returned_by_subscribe_returns_a_200_if_called() {
     let app = spawn_app().await;
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
@@ -43,4 +57,42 @@ async fn the_link_returned_by_subscribe_returns_a_200_if_called() {
         .expect("Failed to perform GET request.");
 
     assert_eq!(response.status().as_u16(), 200);
+}
+
+#[actix_rt::test]
+async fn opening_the_confirmation_link_confirms_a_subscriber() {
+    let app = spawn_app().await;
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+
+    Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .mount(&app.email_server)
+        .await;
+
+    // Subscribe
+    app.post_subscriptions(body.into()).await;
+    let email_request = &app
+        .email_server
+        .received_requests()
+        .await
+        .expect("Failed to query mock server for received reqeusts.")[0];
+    let confirmation_links = app.get_confirmation_links(email_request);
+
+    // Open subscription confirmation link
+    reqwest::get(confirmation_links.html)
+        .await
+        .expect("Failed to perform GET request.")
+        .error_for_status()
+        .expect("Request returned HTTP error status.");
+
+    // Check that subscription is confirmed
+    let saved = sqlx::query!("SELECT email, name, status FROM subscriptions")
+        .fetch_one(&app.db_pool)
+        .await
+        .expect("Failed to fetch saved subscription.");
+
+    assert_eq!(saved.status, "confirmed");
+    assert_eq!(saved.email, "ursula_le_guin@gmail.com");
+    assert_eq!(saved.name, "le guin");
 }
