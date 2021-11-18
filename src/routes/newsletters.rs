8bdfcf865dbcdd9f35b028718ed9ae3de8343d5d
Josh Choo <dev@joshuous.com>
2021-11-18 10:14:20 +0800

    feat: Hash passwords with SHA3-256

--- src/routes/newsletters.rs
+++ src/routes/newsletters.rs
@@ -4,6 +4,7 @@ use actix_http::{
 };
 use actix_web::{web, HttpResponse, ResponseError};
 use anyhow::Context;
+use sha3::Digest;
 use sqlx::PgPool;
 
 use crate::{domain::SubscriberEmail, email_client::EmailClient, routes::error_chain_fmt};
@@ -58,14 +59,16 @@ async fn validate_credentials(
     credentials: Credentials,
     pool: &PgPool,
 ) -> Result<uuid::Uuid, PublishError> {
+    let password_hash_bytes = sha3::Sha3_256::digest(credentials.password.as_bytes());
+    let password_hash_hexadecimal = format!("{:x}", password_hash_bytes);
     let user_id: Option<_> = sqlx::query!(
         r#"
         SELECT user_id
         FROM users
-        WHERE username = $1 AND password = $2
+        WHERE username = $1 AND password_hash = $2
         "#,
         credentials.username,
-        credentials.password
+        password_hash_hexadecimal,
     )
     .fetch_optional(pool)
     .await
